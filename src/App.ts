4632d5cfeba042df025387aacc07c5ed9c317d24
Maicol Battistini <maicolbattistini@live.it>
2021-07-31 11:09:18 +0200

    feat: Added Typescript and ESLint config

---
+++ src/App.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+// noinspection JSUnusedLocalSymbols
+
+import { Inertia } from '@inertiajs/inertia';
+import m from 'mithril';
+
+const page = {
+  component: {
+    view: () => m('div'),
+  },
+  props: {},
+  key: null,
+};
+
+export default function App({
+  initialPage,
+  initialComponent,
+  resolveComponent,
+  titleCallback,
+  onHeadUpdate,
+}) {
+  return {
+    name: 'Inertia',
+    onHeadUpdate: () => () => {},
+    oncreate: () => {
+      const isServer = typeof window === 'undefined';
+      // const headManager = createHeadManager(isServer, titleCallback, onHeadUpdate);
+
+      if (!isServer) {
+        Inertia.init({
+          initialPage,
+          resolveComponent,
+          async swapComponent(args) {
+          // page = args.page;
+          // @ts-ignore
+            page.component = args.component;
+            page.key = args.preserveState ? page.key.value : Date.now();
+          },
+          // @ts-ignore
+          updatePage(component, props, { preserveState }) {
+            page.component = component;
+            page.props = props;
+            page.key = preserveState ? page.key : Date.now();
+            m.redraw();
+          },
+        });
+      }
+    },
+    view: () => m(page.component, page.props),
+
+  };
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as InertiaApp } from './App';
+export { default as createInertiaApp } from './createInertiaApp';
+export { default as InertiaLink } from './link';
